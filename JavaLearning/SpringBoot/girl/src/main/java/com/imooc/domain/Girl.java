d92d5f92ea029aa608afdcd3985421e822fc7c1d
zhengyuyun <2814154894@qq.com>
2018-08-21 13:29:46 +0800

    1

---
+++ JavaLearning/SpringBoot/girl/src/main/java/com/imooc/domain/Girl.java
@@ -0,0 +1,73 @@
+package com.imooc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.awt.geom.Arc2D;
+
+
+@Entity
+public class Girl {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotBlank(message = "cupSize字段必传")
+    private String cupSize;
+
+    @Min(value = 18,message = "未成年少女禁止入内")
+    private Integer age;
+
+    @NotNull(message = "money必传")
+    private Double money;
+
+    public Girl() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", cupSize='" + cupSize + '\'' +
+                ", age=" + age +
+                ", money=" + money +
+                '}';
+    }
+}
---
+++ JavaLearning/SpringBoot/girl/src/main/java/com/imooc/service/GirlService.java
@@ -0,0 +1,45 @@
+package com.imooc.service;
+
+import com.imooc.domain.Girl;
+import com.imooc.enums.ResultEnum;
+import com.imooc.exception.GirlException;
+import com.imooc.repository.GirlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class GirlService {
+
+    @Autowired
+    private GirlRepository girlRepository;
+  @Transactional
+    public void insertTwo(){
+        Girl girlA=new Girl();
+        girlA.setCupSize("A");
+        girlA.setAge((18));
+        girlRepository.save(girlA);
+
+        Girl girlB=new Girl();
+        girlB.setCupSize("BBBB");
+        girlB.setAge(19);
+        girlRepository.save(girlB);
+    }
+
+    public void getAge(Integer id) throws Exception{
+      Girl girl=girlRepository.getOne(id);
+      Integer age=girl.getAge();
+        if (age < 10) {
+            //返回“你还在上小学吧”
+            throw new GirlException(ResultEnum.PRIMARY_SCHOOL);
+        } else if (age > 10 && age < 16) {
+            //返回“你可能还在上高中”
+            throw new GirlException(ResultEnum.MIDDLE_SCHOOL);
+        }
+    }
+
+    public Girl findOne(Integer id) {
+        return girlRepository.getOne(id);
+    }
+}
